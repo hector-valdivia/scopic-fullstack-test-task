5223cc33b293e877733e05a1f294a3e792cce818
Hector Valdivia <hect.valdivia@gmail.com>
2021-05-07 18:23:08 -0600

    Logout route

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ItemAutoBidController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +17,26 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [LoginController::class, 'login']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group( function () {
+    Route::get('logout', [LoginController::class, 'logout']);
+
+    #USER
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/user/{id}', [UserController::class, 'show']);
+    Route::put('/user/{id}', [UserController::class, 'update']);
+    Route::delete('/user/{user}', [UserController::class, 'destroy']);
+    Route::get('/me', [UserController::class, 'show']);
+    Route::put('/me', [UserController::class, 'updateMe']);
+
+    #ITEM
+    Route::get('item', [ItemController::class, 'index']);
+    Route::post('item', [ItemController::class, 'store']);
+    Route::get('item/{item}', [ItemController::class, 'show']);
+    Route::put('item/{item}', [ItemController::class, 'update']);
+    Route::post('item/{item}/bid', [ItemController::class, 'bid']);
+    Route::get('item/{item}/subscribe', [ItemAutoBidController::class, 'subscribe']);
+    Route::get('item/{item}/unsubscribe', [ItemAutoBidController::class, 'unsubscribe']);
 });
